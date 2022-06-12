b95a76bcf6bad962fa84d84094e8f4c529a50fa1
Dhairya <dhairya.ostwal@gmail.com>
2022-06-12 13:41:41 +0530

    Project Setup & Model Creation

---
+++ books/api/models.py
@@ -0,0 +1,12 @@
+from sys import maxsize
+from django.db import models
+
+# Create your models here.
+class BooksAPI(models.Model):
+    title = models.CharField(max_length=100, default="unknown")
+    author = models.CharField(max_length=100, default="unknown")
+    description = models.TextField(max_length=200, default="not available")
+    img_url = models.CharField(max_length=5000)
+
+    class Meta:
+        ordering = ["title"]
