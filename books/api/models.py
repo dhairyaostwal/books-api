fe5027dc773fb8b8e542a77fb2dce386b99eca7e
Dhairya <dhairya.ostwal@gmail.com>
2022-06-13 07:50:34 +0530

    Updated Model

--- books/api/models.py
+++ books/api/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
 class BooksAPI(models.Model):
     title = models.CharField(max_length=100, default="unknown")
     author = models.CharField(max_length=100, default="unknown")
     description = models.TextField(max_length=200, default="not available")
-    img_url = models.CharField(max_length=5000)
+    img_url = models.CharField(max_length=5000, default="not available")
 
     class Meta:
         ordering = ["title"]
